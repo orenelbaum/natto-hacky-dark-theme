cfacdc4bc0176c6ca03b9a5ac67d0b5eec923396
Oren Elbaum <elbaumoren@gmail.com>
2021-10-17 11:56:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+export default globalCss({
+   ".cm-content": {
+      caretColor: "white !important"
+   },
+   ".pane": {
+      ".bg-white, .bg-gray-25": {
+         background: "#050a18"
+      },
+      color: "white"
+   },
+   ".ͼ4 .cm-matchingBracket .cmt-punctuation": {
+      color: "pink"
+   },
+   ".cmt-punctuation": {
+      color: "#9bb"
+   },
+   ".bg-gray-250.bg-grid.absolute.top-0.bottom-0.left-0.right-0": {
+      background: "rgba(0,10,20,0.85)"
+   },
+   ".bg-gray-125.flex.items-center.flex-shrink-0.rounded-t.px-2.select-none.transition-colors.group.h-8.cursor-grab": {
+      background: "#123"
+   },
+   ".px-1.bg-gray-250.bg-opacity-70.text-gray-400": {
+      color: "#123"
+   },
+   ".bg-gray-25.fixed.left-0.top-20.bottom-0.flex.flex-col.transform.transition.z-1": {
+      background: "#020812",
+      color: "pink",
+      "& .bg-blue-100": {
+         background: "#223344",
+         // background: "blue",
+         color: "pink"
+      },
+      "& .bg-gray-100": {
+         background: "#192624",
+         color: "pink"
+      }
+   },
+   ".fixed.left-0.top-0.right-0.h-20.bg-gray-25.flex.items-end.z-1": {
+      background: "black",
+      color: "pink",
+      "& button": {
+         color: "pink"
+      }
+   },
+   ".pl-4.pr-3.flex.items-center.text-gray-500.justify-between.transition-colors": {
+      background: "black",
+      color: "white"
+   },
+   ".px-1.flex.items-center.rounded-sm.transition.bg-gray-50.text-gray-400": {
+      background: "#234",
+      color: "white"
+   },
+   ".cmt-keyword": {
+      color: "#ce93ff"
+   },
+   ".cmt-variableName, .cmt-propertyName": {
+      color: "#d9c9ee"
+   },
+   ".cmt-string": {
+      color: "#c6ffed"
+   },
+   ".cmt-typeName": {
+      color: "#dcff7a"
+   },
+   ".codicon.codicon-window.icon-xs.text-gray-500": {
+      color: "pink"
+   },
+   ".absolute.left-0.top-0.bottom-0.right-0.z-10.cursor-grab.bg-white.bg-opacity-50.rounded": {
+      opacity: 0.4
+   },
+   ".bg-neutral": {
+      background: "black",
+      color: "pink"
+   },
+   "div::-webkit-scrollbar": {
+      width: "1em"
+   },
+   "div::-webkit-scrollbar-thumb": {
+      background: "#234"
+   }
+})
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Natto Hacky Dark Theme
+
+This is a dark theme for [Natto](natto.dev) (an awesome graphical editor for JS). It's pretty hacky, but it's only a temporary solution until Natto adds support for theming.
+
+## How to use
+
+Inisde Natto, import Natto Hacky Dark Theme from npm under the name `natto-hacky-dark-theme`.
+
+In another pane, take the default import, which is a function, and call it. It will apply the theme to the current canvas.
